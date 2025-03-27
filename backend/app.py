aa66aff5065eb07b015e10f48794db30daec7a46
David <88470336+MrKingDev@users.noreply.github.com>
2025-03-27 06:52:46 -0400

    Backend & New pages
    - Added each page that was needed for the html
    
    - Added the backend with python (webview)

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+import webview
+import os
+
+# Get the absolute path to the frontend folder (this helps after packaging too)
+frontend_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'frontend'))
+
+webview.create_window(
+    title="KingDev's Finance App",
+    url=os.path.join(frontend_path, 'index.html'),
+    width=1920,
+    height=1080,
+    min_size=(900, 700),
+    resizable=True
+)
+
+webview.start()
